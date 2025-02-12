3e62ea6234e07944d9c2359c1fbd3fa5e5b7dbb4
bebeji-nappa <masadecade@gmail.com>
2025-02-12 18:44:38 +0900

    fix: 細かいバグ修正
    - blockquote の挙動修正
    - babel 削除 & ts-jest の導入

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   moduleFileExtensions: ["js", "ts"],
+  transform: {
+    "^.+\\.ts$": "ts-jest"
+  },
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.json"
+    }
+  },
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
